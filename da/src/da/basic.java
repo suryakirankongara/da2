70d03815c7b1f0edc733bd0adfcd36c08b105184
suryakirankongara <suryakirankongara@gmail.com>
2023-10-28 21:31:40 +0530

    surya da commit

---
+++ da/src/da/basic.java
@@ -0,0 +1,10 @@
+package da;
+
+public class basic {
+	 public static void main(String[] args) {
+	        System.out.println("Hello, World!"); 
+	        System.out.println("Surya"); 
+	        
+	    }
+
+}
