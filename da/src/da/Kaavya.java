4ca0f66f3b246b5b18cf4a878230fdfd2a8a5a92
kaavya-25 <116670352+kaavya-25@users.noreply.github.com>
2023-10-28 22:16:22 +0530

    Kaavya committed

---
+++ da/src/da/Kaavya.java
@@ -0,0 +1,10 @@
+package da;
+
+public class Kaavya {
+	 public static void main(String[] args) {
+	        System.out.println("Hello, World!"); 
+	        System.out.println("Kaavya"); 
+	        
+	    }
+
+}
